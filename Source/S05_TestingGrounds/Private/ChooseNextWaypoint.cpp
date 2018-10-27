6db25e9e27a14a9e88cd886c7113c99866ad182a
mrgreenh <carloandreaconte@icloud.com>
2018-10-26 21:31:59 -0700

    unparented from patrolling guard

--- Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -3,7 +3,6 @@
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Classes/AIController.h"
-#include "PatrollingGuard.h"
 #include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
@@ -11,7 +10,7 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 
 	AAIController* AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = Cast<APatrollingGuard>(AIController->GetPawn());
+	auto ControlledPawn = AIController->GetPawn();
 	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
 	if (!ensure(PatrolRoute) || PatrolRoute->GetPatrolPoints().Num() <= 0) { return EBTNodeResult::Failed; }
 	auto NextPoint = PatrolRoute->GetPatrolPoints()[Index];
